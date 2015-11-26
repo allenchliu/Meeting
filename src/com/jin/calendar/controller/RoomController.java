cfd156f6fa82c2560054ab63da2afd55ff61fcbe
Allen Liu <allenchliu@hotmail.com>
2015-11-26 22:32:35 +0800

    Translation

--- src/com/jin/calendar/controller/RoomController.java
+++ src/com/jin/calendar/controller/RoomController.java
@@ -10,56 +10,52 @@ import com.jin.calendar.bo.DwzResponseBO;
 import com.jin.calendar.common.CommonConstant;
 import com.jin.calendar.model.Room;
 
-/**
- * 会议室Controller控制类
- * @author JinLiang
- * @datetime 2014年4月28日 下午10:03:18
- */
 public class RoomController extends Controller {
 
-	public void index(){
-		Page<Room> page = Room.dao.paginate(CommonConstant.PAGENUMBER, CommonConstant.PAGESIZE, "select *", "from room");
-		setAttr("page", page);
-		render("room.jsp");
-	}
+    public void index() {
+        Page<Room> page = Room.dao.paginate(CommonConstant.PAGENUMBER, CommonConstant.PAGESIZE, "select *", "from room");
+        setAttr("page", page);
+        render("room.jsp");
+    }
 
-	public void getPageList(){
-		Page<Room> page = Room.dao.paginate(getParaToInt("pageNum"), getParaToInt("numPerPage"), "select *", "from room");
-		setAttr("page", page);
-		render("room.jsp");
-	}
-	
-	public void goAdd(){
-		render("room_add.jsp");
-	}
-	
-	public void add(){
-		DwzResponseBO responseBO = new DwzResponseBO(getPara("navTabId"),"closeCurrent");
-		boolean flag=new Room().set("name", getPara("name")).save();
-		if(!flag){
-			responseBO.setStatusCode("300");
-			responseBO.setMessage("操作失败！");
-		}
-		renderJson(responseBO);
-						
-	}
-	
-	public void del(){
-		DwzResponseBO responseBO = new DwzResponseBO();
-		boolean flag=Db.deleteById("room", getParaToInt());
-		if(!flag){
-			responseBO.setStatusCode("300");
-			responseBO.setMessage("操作失败！");
-		}
-		renderJson(responseBO);
-	}
-	
-	public void isUnique(){
-		List<Record> list = Db.find("select * from room where name= ?", getPara("name"));
-		if(list.size()>0){
-			renderText("false");
-		}else{
-			renderText("true");
-		}
-	}
+    public void getPageList() {
+        Page<Room> page = Room.dao.paginate(getParaToInt("pageNum"), getParaToInt("numPerPage"), "select *", "from room");
+        setAttr("page", page);
+        render("room.jsp");
+    }
+
+    public void goAdd() {
+        render("room_add.jsp");
+    }
+
+    public void add() {
+        DwzResponseBO responseBO = new DwzResponseBO(getPara("navTabId"), "closeCurrent");
+        boolean flag = new Room().set("name", getPara("name")).save();
+        if (!flag) {
+            responseBO.setStatusCode("300");
+            responseBO.setMessage("Error when saving room name.");
+        }
+        renderJson(responseBO);
+
+    }
+
+    public void del() {
+        DwzResponseBO responseBO = new DwzResponseBO();
+        boolean flag = Db.deleteById("room", getParaToInt());
+        if (!flag) {
+            responseBO.setStatusCode("300");
+            responseBO.setMessage("Error when deleting meeting room.");
+        }
+        renderJson(responseBO);
+    }
+
+    public void isUnique() {
+        List<Record> list = Db.find("select * from room where name= ?", getPara("name"));
+        if (list.size() > 0) {
+            renderText("false");
+        }
+        else {
+            renderText("true");
+        }
+    }
 }
--- src/com/jin/calendar/controller/RoomScheduleController.java
+++ src/com/jin/calendar/controller/RoomScheduleController.java
@@ -13,75 +13,69 @@ import com.jin.calendar.model.RoomSchedule;
 
 public class RoomScheduleController extends Controller {
 
-	public void getDurationEvent() {
-		List<RoomSchedule> list = RoomSchedule.dao.getDurationEventsByRoomId(
-				getParaToInt("roomId"), getParaToLong("start"),
-				getParaToLong("end"));
-		List<RoomEvent> events = new ArrayList<>();
-		for (RoomSchedule roomSchedule : list) {
-			RoomEvent event = new RoomEvent(roomSchedule.getInt("id"),
-					roomSchedule.getStr("subject"),
-					roomSchedule.getTimestamp("start"),
-					roomSchedule.getTimestamp("end"),
-					roomSchedule.getStr("username"),
-					roomSchedule.getStr("roomname"),
-					roomSchedule.getStr("email"),
-					false);
-			if (roomSchedule.getTimestamp("start").before(new Date())) {
-				event.setColor(CommonConstant.COLOR_FOR_PAST_EVENT);
-			} else if(roomSchedule.getInt("userid").equals(getSessionAttr("userId"))){
-				event.setColor(CommonConstant.COLOR_FOR_OWN_FUTURE_EVENT);
-				event.setEditable(true);
-			} else {
-				event.setColor(CommonConstant.COLOR_FOR_FUTURE_EVENT);
-			}
-			events.add(event);
-		}
+    public void getDurationEvent() {
+        List<RoomSchedule> list = RoomSchedule.dao.getDurationEventsByRoomId(getParaToInt("roomId"), getParaToLong("start"), getParaToLong("end"));
+        List<RoomEvent> events = new ArrayList<>();
+        for (RoomSchedule roomSchedule : list) {
+            RoomEvent event = new RoomEvent(roomSchedule.getInt("id"), roomSchedule.getStr("subject"), roomSchedule.getTimestamp("start"),
+                    roomSchedule.getTimestamp("end"), roomSchedule.getStr("username"), roomSchedule.getStr("roomname"), roomSchedule.getStr("email"), false);
+            if (roomSchedule.getTimestamp("start").before(new Date())) {
+                event.setColor(CommonConstant.COLOR_FOR_PAST_EVENT);
+            }
+            else if (roomSchedule.getInt("userid").equals(getSessionAttr("userId"))) {
+                event.setColor(CommonConstant.COLOR_FOR_OWN_FUTURE_EVENT);
+                event.setEditable(true);
+            }
+            else {
+                event.setColor(CommonConstant.COLOR_FOR_FUTURE_EVENT);
+            }
+            events.add(event);
+        }
 
-		renderJson(events);
-	}
+        renderJson(events);
+    }
 
-	public void updateRoomEvent(){
-		Map<String, Object> returnMap=new HashMap<>();
-		returnMap.put("isSuccess", true);
-		Date start=new Date(getParaToLong("start"));
-		Date end=new Date(getParaToLong("end"));
-		if(start.before(new Date())){
-			returnMap.put("isSuccess", false);
-			returnMap.put("msg", "调整后的开始时间已过期!");
-		}else if(! RoomSchedule.dao.isLegalEvent(getParaToLong("start")/1000, getParaToLong("end")/1000,getParaToInt("roomId"),getParaToInt("id"))){
-			returnMap.put("isSuccess", false);
-			returnMap.put("msg", "与其他会议时间有冲突！");
-		} else{
-			RoomSchedule roomSchedule=RoomSchedule.dao.findById(getParaToInt("id"));
-			roomSchedule.set("start", start);
-			roomSchedule.set("end", end);
-			roomSchedule.update();
-		}
-		renderJson(returnMap);
-	}
+    public void updateRoomEvent() {
+        Map<String, Object> returnMap = new HashMap<>();
+        returnMap.put("isSuccess", true);
+        Date start = new Date(getParaToLong("start"));
+        Date end = new Date(getParaToLong("end"));
+        if (start.before(new Date())) {
+            returnMap.put("isSuccess", false);
+            returnMap.put("msg", "The start time is already passed. Please choose a new time slot.");
+        }
+        else if (!RoomSchedule.dao.isLegalEvent(getParaToLong("start") / 1000, getParaToLong("end") / 1000, getParaToInt("roomId"), getParaToInt("id"))) {
+            returnMap.put("isSuccess", false);
+            returnMap.put("msg", "Not a legal event. Please check again.");
+        }
+        else {
+            RoomSchedule roomSchedule = RoomSchedule.dao.findById(getParaToInt("id"));
+            roomSchedule.set("start", start);
+            roomSchedule.set("end", end);
+            roomSchedule.update();
+        }
+        renderJson(returnMap);
+    }
 
-	public void addRoomEvent() {
-		Map<String, Object> returnMap = new HashMap<>();
-		returnMap.put("isSuccess", true);
-		Date start = new Date(getParaToLong("start"));
-		Date end = new Date(getParaToLong("end"));
-		String title = getPara("title");
-		int roomId = getParaToInt("roomId");
-		if (start.before(new Date())) {
-			returnMap.put("isSuccess", false);
-			returnMap.put("msg", "开始时间已过期!");
-		} else if(! RoomSchedule.dao.isLegalEvent(getParaToLong("start")/1000, getParaToLong("end")/1000,roomId,-100)){
-			returnMap.put("isSuccess", false);
-			returnMap.put("msg", "与其他会议时间有冲突！");
-		} else {
-			new RoomSchedule().set("start", start)
-					.set("end", end)
-					.set("subject", title)
-					.set("userid", getSessionAttr("userId"))
-					.set("roomid", roomId)
-					.set("create_date", new Date()).save();
-		}
-		renderJson(returnMap);
-	}
+    public void addRoomEvent() {
+        Map<String, Object> returnMap = new HashMap<>();
+        returnMap.put("isSuccess", true);
+        Date start = new Date(getParaToLong("start"));
+        Date end = new Date(getParaToLong("end"));
+        String title = getPara("title");
+        int roomId = getParaToInt("roomId");
+        if (start.before(new Date())) {
+            returnMap.put("isSuccess", false);
+            returnMap.put("msg", "The start time is already passed. Please choose a new time slot.");
+        }
+        else if (!RoomSchedule.dao.isLegalEvent(getParaToLong("start") / 1000, getParaToLong("end") / 1000, roomId, -100)) {
+            returnMap.put("isSuccess", false);
+            returnMap.put("msg", "Not a legal event. Please check again.");
+        }
+        else {
+            new RoomSchedule().set("start", start).set("end", end).set("subject", title).set("userid", getSessionAttr("userId")).set("roomid", roomId)
+                    .set("create_date", new Date()).save();
+        }
+        renderJson(returnMap);
+    }
 }
