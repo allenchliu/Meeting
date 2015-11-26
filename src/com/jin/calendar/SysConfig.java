9138b4b0110e5bdd0478a785ccad8bba599b5505
Allen Liu <allenchliu@hotmail.com>
2015-11-26 21:49:20 +0800

    Fixed DB login issue

--- src/com/jin/calendar/SysConfig.java
+++ src/com/jin/calendar/SysConfig.java
@@ -1,7 +1,5 @@
 package com.jin.calendar;
 
-import java.util.Map;
-
 import com.jfinal.config.Constants;
 import com.jfinal.config.Handlers;
 import com.jfinal.config.Interceptors;
@@ -25,77 +23,67 @@ import com.jin.calendar.model.User;
 import com.jin.calendar.model.UserMenu;
 
 /**
- * 系统配置类，继承JFinalConfig，用于对整个项目进行配置
+ * 绯荤粺閰嶇疆绫伙紝缁ф壙JFinalConfig锛岀敤浜庡鏁翠釜椤圭洰杩涜閰嶇疆
+ * 
  * @author Jin
- * @datetime 2014年1月15日 下午9:24:30
+ * @datetime 2014骞�1鏈�15鏃� 涓嬪崍9:24:30
  */
 public class SysConfig extends JFinalConfig {
 
-	public static String URL = null;
-	public static String USER = null;
-	public static String PW = null;
-	
-	static{
-		Map<String, String> m = System.getenv();
-		URL = "jdbc:mysql://" + m.get("MOPAAS_MYSQL6594_HOST")+":"+m.get("MOPAAS_MYSQL6594_PORT")+ "/" + m.get("MOPAAS_MYSQL6594_NAME") + "?characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull";
-		USER = m.get("MOPAAS_MYSQL6594_USERNAME");
-		PW = m.get("MOPAAS_MYSQL6594_PASSWORD");
-	}
-	
-	@Override
-	public void configConstant(Constants me) {
+    @Override
+    public void configConstant(Constants me) {
 
-//		me.setDevMode(true);//配置当前为开发模式
-		me.setViewType(ViewType.JSP);//配置默认视图为JSP
-		me.setBaseViewPath("/WEB-INF/page");
-		
-		loadPropertyFile("jdbcConfig.properties");//加载数据库连接配置
+        // me.setDevMode(true);//閰嶇疆褰撳墠涓哄紑鍙戞ā寮�
+        me.setViewType(ViewType.JSP);// 閰嶇疆榛樿瑙嗗浘涓篔SP
+        me.setBaseViewPath("/WEB-INF/page");
 
-	}
+        loadPropertyFile("jdbcConfig.properties");// 鍔犺浇鏁版嵁搴撹繛鎺ラ厤缃�
 
-	@Override
-	public void configRoute(Routes me) {
-		me.add("/", CommonController.class);
-		me.add("/user", UserController.class,"/admin");
-		me.add("/menu", MenuController.class,"/admin");
-		me.add("/userMenu", UserMenuController.class);
-		me.add("/room", RoomController.class,"/admin");
-		me.add("/roomSchedule", RoomScheduleController.class);
-		me.add("/admin", AdminController.class);
-		
-	}
+    }
 
-	@Override
-	public void configPlugin(Plugins me) {
-		// 配置Druid数据库连接池插件
-//		DruidPlugin druidPlugin = new DruidPlugin(getProperty("jdbcURL"),
-//				getProperty("jdbcUser"), getProperty("jdbcPassword"));
-		DruidPlugin druidPlugin = new DruidPlugin(URL, USER, PW);
-		me.add(druidPlugin);
+    @Override
+    public void configRoute(Routes me) {
+        me.add("/", CommonController.class);
+        me.add("/user", UserController.class, "/admin");
+        me.add("/menu", MenuController.class, "/admin");
+        me.add("/userMenu", UserMenuController.class);
+        me.add("/room", RoomController.class, "/admin");
+        me.add("/roomSchedule", RoomScheduleController.class);
+        me.add("/admin", AdminController.class);
 
-		// 配置ActiveRecord插件
-		ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
-		me.add(arp);
-//		arp.setContainerFactory(new  CaseInsensitiveContainerFactory());
-		arp.addMapping("menu", Menu.class);
-		arp.addMapping("user", User.class);
-		arp.addMapping("user_menu", UserMenu.class);
-		arp.addMapping("room", Room.class);
-		arp.addMapping("room_schedule", RoomSchedule.class);
-	}
+    }
 
-	@Override
-	public void configInterceptor(Interceptors me) {
-		me.add( new LoginInterceptor());
-	}
+    @Override
+    public void configPlugin(Plugins me) {
+        // 閰嶇疆Druid鏁版嵁搴撹繛鎺ユ睜鎻掍欢
+        // DruidPlugin druidPlugin = new DruidPlugin(getProperty("jdbcURL"),
+        // getProperty("jdbcUser"), getProperty("jdbcPassword"));
+        DruidPlugin druidPlugin = new DruidPlugin(getProperty("jdbcURL"), getProperty("jdbcUser"), getProperty("jdbcPassword"));
+        me.add(druidPlugin);
 
-	@Override
-	public void configHandler(Handlers me) {
-		// TODO Auto-generated method stub
+        // 閰嶇疆ActiveRecord鎻掍欢
+        ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
+        me.add(arp);
+        // arp.setContainerFactory(new CaseInsensitiveContainerFactory());
+        arp.addMapping("menu", Menu.class);
+        arp.addMapping("user", User.class);
+        arp.addMapping("user_menu", UserMenu.class);
+        arp.addMapping("room", Room.class);
+        arp.addMapping("room_schedule", RoomSchedule.class);
+    }
 
-	}
+    @Override
+    public void configInterceptor(Interceptors me) {
+        me.add(new LoginInterceptor());
+    }
 
-//	public static void main(String[] args) {
-//		JFinal.start("WebRoot", 80, "/", 5);
-//	}
+    @Override
+    public void configHandler(Handlers me) {
+        // TODO Auto-generated method stub
+
+    }
+
+    // public static void main(String[] args) {
+    // JFinal.start("WebRoot", 80, "/", 5);
+    // }
 }
